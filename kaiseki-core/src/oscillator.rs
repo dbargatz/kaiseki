21257afa70e8ae0432c8b19d7916723296094aae
Dylan Bargatze <dylan@forallsecure.com>
2022-07-22 06:46:11 -0700

    Correct for clock jitter.

--- kaiseki-core/src/oscillator.rs
+++ kaiseki-core/src/oscillator.rs
@@ -23,10 +23,11 @@ impl Component for Oscillator {
     fn start(&mut self) {
         let freq: f64 = self.frequency_hz as f64;
         let period_secs: f64 = 1.0 / freq;
+        let period_duration = std::time::Duration::from_secs_f64(period_secs);
         println!("starting oscillator with period {}ns", period_secs);
 
         let start_time = std::time::Instant::now();
-        let period = std::time::Duration::from_secs_f64(period_secs);
+        let mut period = period_duration;
 
         let bus = self.bus.as_mut().unwrap();
 
@@ -36,10 +37,18 @@ impl Component for Oscillator {
 
             let period_start = std::time::Instant::now();
             std::thread::sleep(period);
-            self.cycles += 1;
             let period_end = std::time::Instant::now();
+            self.cycles += 1;
+
             let total_elapsed = period_end - start_time;
             let period_elapsed = period_end - period_start;
+            let expected_elapsed = period_duration.mul_f64(self.cycles as f64);
+            if expected_elapsed > total_elapsed {
+                period = period_duration + (expected_elapsed - total_elapsed);
+            } else if expected_elapsed < total_elapsed {
+                let diff = total_elapsed - expected_elapsed;
+                period = period_duration.saturating_sub(diff);
+            }
 
             println!(
                 "last tick elapsed: {}ns (total: {} secs)",
