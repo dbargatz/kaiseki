6d50aacac9bdf1079f602d73b65221f58d537a9a
Dylan Bargatze <dylan@forallsecure.com>
2022-07-22 07:27:41 -0700

    Move the Chip-8 program into the source tree.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
     tracing::info!("loading Chip-8 program");
-    let program = fs::read("/Users/dylan/Downloads/Chip8 Picture.ch8").unwrap();
+    let program = fs::read("kaiseki-chip8/assets/Chip8 Picture.ch8").unwrap();
     let mut machine = Chip8Machine::new(&program).unwrap();
     machine.start();
     Ok(())
