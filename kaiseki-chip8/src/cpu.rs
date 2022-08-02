d01a4baa5606e3f4e5ce8e486d4f4a26e6f68ea9
Dylan Bargatze <dbargatz@users.noreply.github.com>
2022-08-01 20:56:53 -0400

    Implement hacky opcode handling for most opcodes.

--- kaiseki-chip8/src/cpu.rs
+++ kaiseki-chip8/src/cpu.rs
@@ -1,3 +1,5 @@
+use std::u8;
+
 use anyhow::Result;
 use async_trait::async_trait;
 use bytes::Buf;
@@ -58,22 +60,248 @@ impl Chip8CPU {
         cpu
     }
 
+    async fn fetch(&self, address: u16) -> Result<u16> {
+        let mut bytes = self.memory_bus.read(&self.id, address as usize, 2).await?;
+        Ok(bytes.get_u16())
+    }
+
     async fn execute_cycle(&mut self, cycle_number: usize) -> Result<()> {
-        let offset = (2 * cycle_number) % 0x800;
-        let address = 0x200 + offset;
-        let instruction_bytes = self
-            .memory_bus
-            .read(&self.id, address, 2)
-            .await
-            .unwrap()
-            .get_u16();
+        let opcode = self.fetch(self.regs.PC).await?;
+        let embedded_address = opcode & 0x0FFF;
+        let embedded_byte = (opcode & 0x00FF) as u8;
+        let embedded_nybble = (opcode & 0x000F) as u8;
+        let vx_id = ((opcode & 0x0F00) >> 8) as u8;
+        let vy_id = ((opcode & 0x00F0) >> 4) as u8;
+        let desc: String;
+        match opcode {
+            0x0000..=0x0FFF => match opcode {
+                0x00E0 => {
+                    self.regs.PC += 2;
+                    desc = String::from("clear screen");
+                }
+                0x00EE => {
+                    self.regs.PC = self.stack.pop();
+                    desc = String::from("return from subroutine");
+                }
+                _ => {
+                    self.regs.PC = embedded_address;
+                    desc = format!(
+                        "execute machine language subroutine at 0x{:04X}",
+                        embedded_address
+                    );
+                }
+            },
+            0x1000..=0x1FFF => {
+                self.regs.PC = embedded_address;
+                desc = format!("jump to address 0x{:04X}", embedded_address);
+            }
+            0x2000..=0x2FFF => {
+                self.stack.push(self.regs.PC + 2);
+                self.regs.PC = embedded_address;
+                desc = format!("execute subroutine at address 0x{:04X}", embedded_address);
+            }
+            0x3000..=0x3FFF => {
+                let vx = self.regs.get_register_ref(vx_id);
+                if *vx == embedded_byte {
+                    self.regs.PC += 4;
+                } else {
+                    self.regs.PC += 2;
+                }
+                desc = format!(
+                    "skip next instruction if V{} == 0x{:02X}",
+                    vx_id, embedded_byte
+                );
+            }
+            0x4000..=0x4FFF => {
+                let vx = self.regs.get_register_ref(vx_id);
+                if *vx != embedded_byte {
+                    self.regs.PC += 4;
+                } else {
+                    self.regs.PC += 2;
+                }
+                desc = format!(
+                    "skip next instruction if V{} != 0x{:02X}",
+                    vx_id, embedded_byte
+                );
+            }
+            0x5000..=0x5FFF => match embedded_nybble {
+                0x0 => {
+                    let vx = self.regs.get_register_ref(vx_id);
+                    let vy = self.regs.get_register_ref(vy_id);
+                    if *vx == *vy {
+                        self.regs.PC += 4;
+                    } else {
+                        self.regs.PC += 2;
+                    }
+                    desc = format!("skip next instruction if V{} == V{}", vx_id, vy_id);
+                }
+                _ => panic!("invalid 0x5XY0 opcode"),
+            },
+            0x6000..=0x6FFF => {
+                let vx = self.regs.get_register_mut(vx_id);
+                *vx = embedded_byte;
+                self.regs.PC += 2;
+                desc = format!("store 0x{:02X} in V{}", embedded_byte, vx_id);
+            }
+            0x7000..=0x7FFF => {
+                let vx = self.regs.get_register_mut(vx_id);
+                *vx += embedded_byte;
+                self.regs.PC += 2;
+                desc = format!("add 0x{:02X} to V{}", embedded_byte, vx_id);
+            }
+            0x8000..=0x8FFF => match embedded_nybble {
+                0x0 => {
+                    let vy = self.regs.get_register_ref(vy_id);
+                    let vy_value = *vy;
+                    let vx = self.regs.get_register_mut(vx_id);
+                    *vx = vy_value;
+                    self.regs.PC += 2;
+                    desc = format!("store V{} in V{}", vy_id, vx_id);
+                }
+                0x1 => {
+                    let vy = self.regs.get_register_ref(vy_id);
+                    let vy_value = *vy;
+                    let vx = self.regs.get_register_mut(vx_id);
+                    *vx |= vy_value;
+                    self.regs.PC += 2;
+                    desc = format!("store V{} | V{} in V{}", vx_id, vy_id, vx_id);
+                }
+                0x2 => {
+                    let vy = self.regs.get_register_ref(vy_id);
+                    let vy_value = *vy;
+                    let vx = self.regs.get_register_mut(vx_id);
+                    *vx &= vy_value;
+                    self.regs.PC += 2;
+                    desc = format!("store V{} & V{} in V{}", vx_id, vy_id, vx_id);
+                }
+                0x3 => {
+                    let vy = self.regs.get_register_ref(vy_id);
+                    let vy_value = *vy;
+                    let vx = self.regs.get_register_mut(vx_id);
+                    *vx ^= vy_value;
+                    self.regs.PC += 2;
+                    desc = format!("store V{} ^ V{} in V{}", vx_id, vy_id, vx_id);
+                }
+                0x4 => {
+                    let vy = self.regs.get_register_ref(vy_id);
+                    let vy_value = *vy;
+                    let vx = self.regs.get_register_mut(vx_id);
+                    let result = *vx as u16 + vy_value as u16;
+                    *vx = result as u8;
+                    if result > u8::MAX as u16 {
+                        self.regs.VF = 0x01;
+                    } else {
+                        self.regs.VF = 0x00;
+                    }
+                    self.regs.PC += 2;
+                    desc = format!("add V{} + V{} with carry", vx_id, vy_id);
+                }
+                0x5 => {
+                    let vy = self.regs.get_register_ref(vy_id);
+                    let vy_value = *vy;
+                    let vx = self.regs.get_register_mut(vx_id);
+                    let borrow = vy_value > *vx;
+                    let result = *vx as u16 - vy_value as u16;
+                    *vx = result as u8;
+                    if borrow {
+                        self.regs.VF = 0x01;
+                    } else {
+                        self.regs.VF = 0x00;
+                    }
+                    self.regs.PC += 2;
+                    desc = format!("subtract V{} - V{} with borrow", vx_id, vy_id);
+                }
+                0x6 => {
+                    let vy = self.regs.get_register_ref(vy_id);
+                    let vy_value = *vy;
+                    let vx = self.regs.get_register_mut(vx_id);
+                    let lsb = vy_value & 0x01;
+                    *vx = vy_value >> 1;
+                    self.regs.VF = lsb;
+                    self.regs.PC += 2;
+                    desc = format!("store V{} >> 1 in V{} with lsb in VF", vy_id, vx_id);
+                }
+                0x7 => {
+                    let vy = self.regs.get_register_ref(vy_id);
+                    let vy_value = *vy;
+                    let vx = self.regs.get_register_mut(vx_id);
+                    let borrow = *vx > vy_value;
+                    let result = vy_value as u16 - *vx as u16;
+                    *vx = result as u8;
+                    if borrow {
+                        self.regs.VF = 0x01;
+                    } else {
+                        self.regs.VF = 0x00;
+                    }
+                    self.regs.PC += 2;
+                    desc = format!("subtract V{} - V{} with borrow", vy_id, vx_id);
+                }
+                0xE => {
+                    let vy = self.regs.get_register_ref(vy_id);
+                    let vy_value = *vy;
+                    let vx = self.regs.get_register_mut(vx_id);
+                    let msb = (vy_value & 0x80) >> 8;
+                    *vx = vy_value << 1;
+                    self.regs.VF = msb;
+                    self.regs.PC += 2;
+                    desc = format!("store V{} << 1 in V{} with msb in VF", vy_id, vx_id);
+                }
+                _ => panic!("invalid 0x8XYN opcode"),
+            },
+            0x9000..=0x9FFF => match embedded_nybble {
+                0x0 => {
+                    let vx = self.regs.get_register_ref(vx_id);
+                    let vy = self.regs.get_register_ref(vy_id);
+                    if *vx != *vy {
+                        self.regs.PC += 4;
+                    } else {
+                        self.regs.PC += 2;
+                    }
+                    desc = format!("skip next instruction if V{} != V{}", vx_id, vy_id);
+                }
+                _ => panic!("invalid 0x9XY0 opcode"),
+            },
+            0xA000..=0xAFFF => {
+                self.regs.VI = self.fetch(embedded_address).await?;
+                self.regs.PC += 2;
+                desc = format!("store memory at 0x{:04X} in VI", embedded_address);
+            }
+            0xB000..=0xBFFF => {
+                self.regs.PC = embedded_address + self.regs.V0 as u16;
+                desc = format!("jump to address 0x{:04X} + V0", embedded_address);
+            }
+            0xC000..=0xCFFF => {
+                let vx = self.regs.get_register_mut(vx_id);
+                // TODO: make the 0x42 random
+                *vx = 0x42 & embedded_byte;
+                self.regs.PC += 2;
+                desc = format!(
+                    "set V{} to a random number with mask 0x{:02X}",
+                    vx_id, embedded_byte
+                );
+            }
+            0xD000..=0xDFFF => {
+                let _vx = self.regs.get_register_ref(vx_id);
+                let _vy = self.regs.get_register_ref(vy_id);
+                // TODO: implement display
+                // let self.regs.VF = self.display_bus.draw(vx, vy, self.regs.VI, embedded_nybble).await?;
+                self.regs.PC += 2;
+                desc = format!(
+                    "draw {}-byte sprite in memory at 0x{:04X} to (V{}, V{})",
+                    embedded_nybble, self.regs.VI, vx_id, vy_id
+                );
+            }
+            _ => panic!("invalid opcode: 0x{:04X}", opcode),
+        }
+
         tracing::debug!(
-            "cycle {} | load 0x{:04X} => 0x{:04X} | stack: {:?}",
+            "cycle {} | PC: 0x{:04X} | 0x{:04X} {}",
             cycle_number,
-            address,
-            instruction_bytes,
-            self.stack
+            self.regs.PC,
+            opcode,
+            desc,
         );
+
         Ok(())
     }
 }
--- kaiseki-chip8/src/registers.rs
+++ kaiseki-chip8/src/registers.rs
@@ -37,4 +37,48 @@ impl Chip8Registers {
             ..Default::default()
         }
     }
+
+    pub fn get_register_ref(&self, index: u8) -> &u8 {
+        match index {
+            0x0 => &self.V0,
+            0x1 => &self.V1,
+            0x2 => &self.V2,
+            0x3 => &self.V3,
+            0x4 => &self.V4,
+            0x5 => &self.V5,
+            0x6 => &self.V6,
+            0x7 => &self.V7,
+            0x8 => &self.V8,
+            0x9 => &self.V9,
+            0xA => &self.VA,
+            0xB => &self.VB,
+            0xC => &self.VC,
+            0xD => &self.VD,
+            0xE => &self.VE,
+            0xF => &self.VF,
+            _ => panic!("invalid register index 0x{:02X}", index),
+        }
+    }
+
+    pub fn get_register_mut(&mut self, index: u8) -> &mut u8 {
+        match index {
+            0x0 => &mut self.V0,
+            0x1 => &mut self.V1,
+            0x2 => &mut self.V2,
+            0x3 => &mut self.V3,
+            0x4 => &mut self.V4,
+            0x5 => &mut self.V5,
+            0x6 => &mut self.V6,
+            0x7 => &mut self.V7,
+            0x8 => &mut self.V8,
+            0x9 => &mut self.V9,
+            0xA => &mut self.VA,
+            0xB => &mut self.VB,
+            0xC => &mut self.VC,
+            0xD => &mut self.VD,
+            0xE => &mut self.VE,
+            0xF => &mut self.VF,
+            _ => panic!("invalid register index 0x{:02X}", index),
+        }
+    }
 }
