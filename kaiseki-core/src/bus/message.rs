2ba9770315dd369f7211fe868155d0ffe7b5df30
Dylan Bargatze <dbargatz@users.noreply.github.com>
2022-08-09 16:36:03 -0400

    Fix RwLock scopes, test request().

--- kaiseki-core/src/bus/message.rs
+++ kaiseki-core/src/bus/message.rs
@@ -33,26 +33,33 @@ struct MessageBusState<M: BusMessage> {
     senders: HashMap<ComponentId, Vec<(ComponentId, Sender<MessageEnvelope<M>>)>>,
 }
 
-pub struct MessageBusConnection<'a, M: BusMessage> {
-    bus: &'a MessageBus<M>,
-    component_id: &'a ComponentId,
+#[derive(Clone)]
+pub struct MessageBusConnection<M: BusMessage> {
+    bus: MessageBus<M>,
+    component_id: ComponentId,
 }
 
-impl<'a, M: BusMessage> MessageBusConnection<'a, M> {
+impl<M: BusMessage> fmt::Debug for MessageBusConnection<M> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "MessageBusConnection[{}]", self.component_id)
+    }
+}
+
+impl<M: BusMessage> MessageBusConnection<M> {
     pub async fn recv(&self) -> Result<(M, Option<oneshot::Sender<M>>), MessageBusError> {
-        self.bus.recv(self.component_id).await
+        self.bus.recv(&self.component_id).await
     }
 
     pub async fn request(&self, request: M) -> Result<M, MessageBusError> {
-        self.bus.request(self.component_id, request).await
+        self.bus.request(&self.component_id, request).await
     }
 
     pub async fn send(&self, message: M) -> Result<(), MessageBusError> {
-        self.bus.send(self.component_id, message).await
+        self.bus.send(&self.component_id, message).await
     }
 
     pub fn try_recv(&self) -> Result<(M, Option<oneshot::Sender<M>>), MessageBusError> {
-        self.bus.try_recv(self.component_id)
+        self.bus.try_recv(&self.component_id)
     }
 }
 
@@ -68,6 +75,12 @@ impl<M: BusMessage> Component for MessageBus<M> {
     }
 }
 
+impl<M: BusMessage> fmt::Debug for MessageBus<M> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "MessageBus[{}]", self.id)
+    }
+}
+
 impl<M: BusMessage> MessageBus<M> {
     pub fn new(name: &str) -> Self {
         let state = MessageBusState {
@@ -80,31 +93,26 @@ impl<M: BusMessage> MessageBus<M> {
         }
     }
 
-    pub fn connect<'a>(
-        &'a self,
-        sender_id: &'a ComponentId,
-        receiver_id: &'a ComponentId,
-    ) -> Result<(MessageBusConnection<'a, M>, MessageBusConnection<'a, M>)> {
+    pub fn connect(
+        &self,
+        sender_id: &ComponentId,
+        receiver_id: &ComponentId,
+    ) -> Result<(MessageBusConnection<M>, MessageBusConnection<M>)> {
         let (tx_sender_to_receiver, rx_receiver_from_sender) = async_channel::unbounded();
 
-        {
-            let mut state = self
-                .state
-                .write()
-                .expect("MessageBus state lock was poisoned");
-            let receiver_entry = state.receivers.entry(receiver_id.clone()).or_default();
-            receiver_entry.push((sender_id.clone(), rx_receiver_from_sender));
-            let sender_entry = state.senders.entry(sender_id.clone()).or_default();
-            sender_entry.push((receiver_id.clone(), tx_sender_to_receiver));
-        }
+        let mut state = self.state.write().unwrap();
+        let receiver_entry = state.receivers.entry(receiver_id.clone()).or_default();
+        receiver_entry.push((sender_id.clone(), rx_receiver_from_sender));
+        let sender_entry = state.senders.entry(sender_id.clone()).or_default();
+        sender_entry.push((receiver_id.clone(), tx_sender_to_receiver));
 
         let sender_connection = MessageBusConnection::<M> {
-            bus: self,
-            component_id: sender_id,
+            bus: self.clone(),
+            component_id: sender_id.clone(),
         };
         let receiver_connection = MessageBusConnection::<M> {
-            bus: self,
-            component_id: receiver_id,
+            bus: self.clone(),
+            component_id: receiver_id.clone(),
         };
         Ok((sender_connection, receiver_connection))
     }
@@ -113,20 +121,21 @@ impl<M: BusMessage> MessageBus<M> {
         &self,
         receiver_id: &ComponentId,
     ) -> Result<(M, Option<oneshot::Sender<M>>), MessageBusError> {
-        let state = self
-            .state
-            .read()
-            .expect("MessageBus state lock was poisoned in recv()");
-        let receivers = state
-            .receivers
-            .get(receiver_id)
-            .ok_or(MessageBusError::NoSendersToReceiver(receiver_id.clone()))?;
+        let receivers;
+        {
+            let state = self.state.read().unwrap();
+            receivers = state
+                .receivers
+                .get(receiver_id)
+                .ok_or(MessageBusError::NoSendersToReceiver(receiver_id.clone()))?
+                .clone();
+        }
 
         let mut futures = FuturesUnordered::new();
         for (sender_id, rx) in receivers {
-            futures.push(async {
+            futures.push(async move {
                 let result = rx.recv().await;
-                (sender_id.clone(), result)
+                (sender_id, result)
             });
         }
 
@@ -140,7 +149,7 @@ impl<M: BusMessage> MessageBus<M> {
                         Ok((message.request, message.response_tx))
                     }
                     Err(_) => Err(MessageBusError::Disconnected(
-                        sender_id,
+                        sender_id.clone(),
                         receiver_id.clone(),
                     )),
                 }
@@ -149,14 +158,15 @@ impl<M: BusMessage> MessageBus<M> {
     }
 
     pub async fn request(&self, sender_id: &ComponentId, request: M) -> Result<M, MessageBusError> {
-        let state = self
-            .state
-            .read()
-            .expect("MessageBus state lock was poisoned in send()");
-        let senders = state
-            .senders
-            .get(sender_id)
-            .ok_or(MessageBusError::NoReceiversForSender(sender_id.clone()))?;
+        let senders;
+        {
+            let state = self.state.read().unwrap();
+            senders = state
+                .senders
+                .get(sender_id)
+                .ok_or(MessageBusError::NoReceiversForSender(sender_id.clone()))?
+                .clone();
+        }
 
         let mut futures = FuturesUnordered::new();
 
@@ -175,7 +185,7 @@ impl<M: BusMessage> MessageBus<M> {
                     ))
                 }
             }
-            futures.push(async {
+            futures.push(async move {
                 let response = responder_rx.await;
                 (receiver_id.clone(), response)
             });
@@ -200,14 +210,16 @@ impl<M: BusMessage> MessageBus<M> {
     }
 
     pub async fn send(&self, sender_id: &ComponentId, message: M) -> Result<(), MessageBusError> {
-        let state = self
-            .state
-            .read()
-            .expect("MessageBus state lock was poisoned in send()");
-        let senders = state
-            .senders
-            .get(sender_id)
-            .ok_or(MessageBusError::NoReceiversForSender(sender_id.clone()))?;
+        let senders;
+        {
+            let state = self.state.read().unwrap();
+            senders = state
+                .senders
+                .get(sender_id)
+                .ok_or(MessageBusError::NoReceiversForSender(sender_id.clone()))?
+                .clone();
+        }
+
         for (receiver_id, tx) in senders {
             let envelope = MessageEnvelope::<M> {
                 response_tx: None,
@@ -230,14 +242,15 @@ impl<M: BusMessage> MessageBus<M> {
         &self,
         receiver_id: &ComponentId,
     ) -> Result<(M, Option<oneshot::Sender<M>>), MessageBusError> {
-        let state = self
-            .state
-            .read()
-            .expect("MessageBus state lock was poisoned in recv()");
-        let receivers = state
-            .receivers
-            .get(receiver_id)
-            .ok_or(MessageBusError::NoSendersToReceiver(receiver_id.clone()))?;
+        let receivers;
+        {
+            let state = self.state.read().unwrap();
+            receivers = state
+                .receivers
+                .get(receiver_id)
+                .ok_or(MessageBusError::NoSendersToReceiver(receiver_id.clone()))?
+                .clone();
+        }
 
         for (sender_id, rx) in receivers {
             match rx.try_recv() {
@@ -320,8 +333,8 @@ mod tests {
         let (a_conn, b_conn) = bus
             .connect(a.id(), b.id())
             .expect("couldn't connect from a to b");
-        assert_eq!(a_conn.component_id, a.id());
-        assert_eq!(b_conn.component_id, b.id());
+        assert_eq!(&a_conn.component_id, a.id());
+        assert_eq!(&b_conn.component_id, b.id());
 
         let state = bus.state.read().unwrap();
         let sender_value = state
@@ -452,5 +465,49 @@ mod tests {
             e_conn.try_recv().unwrap_err(),
             MessageBusError::NoMessagesAvailable(e.id().clone())
         );
+    }
+
+    #[tokio::test]
+    async fn request_works() {
+        let ([a, b, c, _, _], bus) = setup();
+
+        // Connect the three components to the bus such that:
+        // `bus`
+        //   ├─ `a`
+        //   │   ├─ `b`
+        //   │   ├─ `c`
+        let (a_conn, b_conn) = bus
+            .connect(a.id(), b.id())
+            .expect("couldn't connect from a to b");
+        let (_, c_conn) = bus
+            .connect(a.id(), c.id())
+            .expect("couldn't connect from a to c");
+
+        // Ensure that a request sent from `a_conn` can be responded to by `b_conn`.
+        let a_request = TestMessage {
+            contents: String::from("request from a"),
+        };
+        let a_task_conn = a_conn.clone();
+        let a_task_request = a_request.clone();
+        let a_task =
+            tokio::spawn(async move { a_task_conn.request(a_task_request).await.unwrap() });
+
+        let (b_request, b_responder) = b_conn.recv().await.unwrap();
+        assert_eq!(a_request.clone(), b_request);
+        let b_responder = b_responder.unwrap();
+        assert!(!b_responder.is_canceled());
+        let b_response = TestMessage {
+            contents: String::from("response from b"),
+        };
+        b_responder.send(b_response.clone()).unwrap();
+
+        let a_response = a_task.await.unwrap();
+        assert_eq!(a_response, b_response);
+
+        // Ensure that `c_conn` sees the same request from `a_conn`, but also sees the
+        // responder provided by `a_conn` as cancelled/dropped.
+        let (c_request, c_responder) = c_conn.recv().await.unwrap();
+        assert_eq!(a_request.clone(), c_request);
+        assert!(c_responder.unwrap().is_canceled());
     }
 }
