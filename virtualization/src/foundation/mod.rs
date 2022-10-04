bb4c9a12a4877e4b9d4ef72537393642b0407ab6
Dylan Bargatze <dbargatz@users.noreply.github.com>
2022-10-04 08:05:06 -0400

    Break out a separate DispatchQueue implementation.

--- virtualization/src/foundation/mod.rs
+++ virtualization/src/foundation/mod.rs
@@ -1,9 +1,11 @@
 mod dictionary;
+mod dispatch;
 mod error;
 mod string;
 mod url;
 
 pub use dictionary::NSDictionary;
+pub use dispatch::DispatchQueue;
 pub use error::NSError;
 pub use string::NSString;
 pub use url::NSURL;
