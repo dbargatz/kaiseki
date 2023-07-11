cb224a1688e11c42a5b5aa4b973a0537ed07acd0
Dylan Bargatze <dbargatz@users.noreply.github.com>
2023-07-11 15:31:48 +0100

    No longer require machine to be Mutexed.

--- kaiseki-core/src/vex.rs
+++ kaiseki-core/src/vex.rs
@@ -1,34 +1,33 @@
 use std::sync::Arc;
 
 use anyhow::Result;
-use tokio::sync::Mutex;
 
 use crate::machine::Machine;
 
 #[derive(Clone)]
 pub struct Vex {
     command: String,
-    machine: Arc<Mutex<dyn Machine>>,
+    machine: Arc<dyn Machine>,
 }
 
 impl Vex {
     pub fn create(machine: impl Machine, command: &str) -> Self {
         Self {
             command: String::from(command),
-            machine: Arc::new(Mutex::new(machine)),
+            machine: Arc::new(machine),
         }
     }
 
     pub async fn destroy(&self) {}
 
     pub fn get_frame(&self) -> Vec<u8> {
-        self.machine.blocking_lock().get_frame()
+        self.machine.get_frame()
     }
 
     pub async fn revert(&self) {}
     pub async fn snapshot(&self) {}
     pub async fn start(&self) -> Result<()> {
-        let machine = self.machine.lock().await;
+        let machine = &self.machine;
         machine.load(self.command.as_str())?;
         machine.start().await;
         Ok(())
