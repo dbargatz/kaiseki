10510babcc060cf450e49cedf9b1dc2116205ef8
Dylan Bargatze <dbargatz@users.noreply.github.com>
2023-12-16 12:37:35 -0500

    Add existing kaiseki-cpu changes, likely broken.

--- kaiseki-cpu/src/error.rs
+++ kaiseki-cpu/src/error.rs
@@ -2,6 +2,12 @@ use thiserror::Error;
 
 #[derive(Debug, Error, PartialEq)]
 pub enum DisassemblyError {
+    #[error("value {0} violates field {1} constraint")]
+    ConstraintViolated(&'static str, usize),
+
+    #[error("no field with name {0} has been defined")]
+    NoSuchField(String),
+
     #[error("sequence of bytes does not map to a known instruction")]
     UndefinedInstruction,
 }
--- kaiseki-cpu/src/lib.rs
+++ kaiseki-cpu/src/lib.rs
@@ -1,175 +1,84 @@
-use std::fmt;
-
-use bitvec::prelude::*;
+// use bitvec::prelude::*;
 
 mod error;
-use error::{DisassemblyError, Result};
-
 mod field;
-use field::Field;
-
-#[derive(PartialEq)]
-struct InstructionVariantDefinition {
-    name: String,
-    fields: Vec<Field>,
-}
-
-impl InstructionVariantDefinition {
-    pub fn new(name: &str) -> Self {
-        Self {
-            name: String::from(name),
-            fields: Vec::new(),
-        }
-    }
-
-    pub fn add_field(&mut self, field: Field) {
-        self.fields.push(field);
-    }
-
-    pub fn try_disassemble(&self, data: &[u8]) -> Result<()> {
-        let mut stream = data.view_bits::<Lsb0>();
-        for field in &self.fields {
-            if let Err(err) = field.try_disassemble(stream) {
-                return Err(err);
-            }
-            stream = &stream[field.width_bits()..];
-        }
-        Ok(())
-    }
-}
-
-impl fmt::Debug for InstructionVariantDefinition {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut substr = String::new();
-        for field in &self.fields {
-            substr.push_str(&format!("[{:?}] ", field));
-        }
-        f.write_fmt(format_args!("{}: {}", self.name, substr.trim_end()))
-    }
-}
-
-#[derive(Debug, Default)]
-struct InstructionSet {
-    instructions: Vec<InstructionDefinition>,
-}
-
-impl InstructionSet {
-    pub fn new() -> Self {
-        Default::default()
-    }
-
-    pub fn add_instruction_definition(&mut self, instruction: InstructionDefinition) {
-        self.instructions.push(instruction);
-    }
-
-    pub fn disassemble(&self, data: &[u8]) -> Result<()> {
-        for instruction in &self.instructions {
-            if let Ok(_) = instruction.disassemble(data) {
-                return Ok(());
-            }
-        }
-        Err(DisassemblyError::UndefinedInstruction)
-    }
-}
-
-#[derive(Debug, PartialEq)]
-struct InstructionDefinition {
-    name: String,
-    variants: Vec<InstructionVariantDefinition>,
-}
-
-impl InstructionDefinition {
-    pub fn new(name: &str) -> Self {
-        Self {
-            name: String::from(name),
-            variants: Vec::new(),
-        }
-    }
-
-    pub fn add_variant_definition(&mut self, variant: InstructionVariantDefinition) {
-        self.variants.push(variant);
-    }
-
-    pub fn disassemble(&self, data: &[u8]) -> Result<()> {
-        for variant in &self.variants {
-            if let Ok(_) = variant.try_disassemble(data) {
-                return Ok(());
-            }
-        }
-        Err(DisassemblyError::UndefinedInstruction)
-    }
-}
-
 
 #[cfg(test)]
 mod tests {
-    use rangemap::RangeInclusiveMap;
+    // use std::collections::HashMap;
 
-    use crate::{DisassemblyError, InstructionDefinition, InstructionSet, InstructionVariantDefinition, field::{Field, FieldDefinition}};
+    // use crate::{DisassemblyError, field::{Field, FieldValueConstraint}};
 
-    const PUSH_IMM_16: &[u8] = &[0xFF, 0b00_110_000, 0xAA, 0xBB];
-    const RESERVED_OPCODE: &[u8] = &[0x0F, 0x04]; // 2-byte reserved opcode
+    // const PUSH_IMM_16: &[u8] = &[0xFF, 0b00_110_000, 0xAA, 0xBB];
+    // const RESERVED_OPCODE: &[u8] = &[0x0F, 0x04]; // 2-byte reserved opcode
 
-    fn create_push_imm16(opcode8: &FieldDefinition, modrm: &FieldDefinition, r#mod: &FieldDefinition, reg_opcode: &FieldDefinition, reg_memory: &FieldDefinition, imm16: &FieldDefinition) -> InstructionVariantDefinition {
-        let mut push_imm_16 = InstructionVariantDefinition::new("push [imm16]");
-        let f1 = Field::SpecificValue(opcode8.clone(), 0xFF);
-        let mut f2 = Field::Subfields(modrm.clone(), RangeInclusiveMap::new());
-        f2.add_subfield(6..=7, Field::AnyValue(r#mod.clone()));
-        f2.add_subfield(3..=5, Field::SpecificValue(reg_opcode.clone(), 0b110));
-        f2.add_subfield(0..=2, Field::AnyValue(reg_memory.clone()));
-        let f3 = Field::AnyValue(imm16.clone());
+    // fn create_push_imm16(fields: &HashMap<&str, FieldDefinition>) -> InstructionVariantDefinition {
+    //     let mut push_imm_16 = InstructionVariantDefinition::new("push [imm16]");
+    //     push_imm_16.add_field(fields, "opcode8", FieldValueRequirement::Specific(0xFF));
+    //     push_imm_16.add_field(fields, "mod_rm", FieldValueRequirement::Any);
+    //     push_imm_16.restrict_field(fields, "mod_rm.reg_opcode", FieldValueRequirement::Specific(0b110));
+    //     push_imm_16.add_field(fields, "imm16", FieldValueRequirement::Any);
+    //     push_imm_16
+    // }
 
-        push_imm_16.add_field(f1);
-        push_imm_16.add_field(f2);
-        push_imm_16.add_field(f3);
-        push_imm_16
-    }
+    // fn create_isa() -> (HashMap<&'static str, FieldDefinition>, InstructionVariantDefinition) {
+    //     // regex pattern: "[prefix]* [opcode]{1,3} [modrm]? [sib]? [imm]? [disp]?";
+    //     // prefix = 0x26, 0x36, 0x64, 0x65, 0x66, 0x67, 0xF0, 0xF2, 0xF3, 
+    //     // opcode = 1-byte:                0x00 - 0x0E, 0x10 - 0xFF
+    //     //          2-byte: 0x0F      then 0x00 - 0x37, 0x3B - 0xFF
+    //     //          3-byte: 0x0F 0x38 then 0x00 - 0xFF
+    //     //                  0x0F 0x3A then 0x00 - 0xFF
+    //     // modrm pattern: "xx yyy zzz", x: mod, y: reg/opcode, z: reg/mem
+    //     // sib pattern:   "xx yyy zzz", x: scale, y: index, z: base
+    //     // imm pattern:   1, 2, 4, or 8 bytes
+    //     // disp pattern:  1, 2, 4, or 8 bytes
+        
+    //     //let mut x86_isa = InstructionSet::new();
 
-    fn create_isa() -> InstructionSet {
-        // regex pattern: "[prefix]* [opcode]{1,3} [modrm]? [sib]? [imm]? [disp]?";
-        // prefix = 0x26, 0x36, 0x64, 0x65, 0x66, 0x67, 0xF0, 0xF2, 0xF3, 
-        // opcode = 1-byte:                0x00 - 0x0E, 0x10 - 0xFF
-        //          2-byte: 0x0F      then 0x00 - 0x37, 0x3B - 0xFF
-        //          3-byte: 0x0F 0x38 then 0x00 - 0xFF
-        //                  0x0F 0x3A then 0x00 - 0xFF
-        // modrm pattern: "xx yyy zzz", x: mod, y: reg/opcode, z: reg/mem
-        // sib pattern:   "xx yyy zzz", x: scale, y: index, z: base
-        // imm pattern:   1, 2, 4, or 8 bytes
-        // disp pattern:  1, 2, 4, or 8 bytes
-        let mut x86_isa = InstructionSet::new();
+    //     let mut modrm = FieldDefinition::new("mod_rm", 8);
+    //     let r#mod = FieldDefinition::new("mod", 2);
+    //     let reg_opcode = FieldDefinition::new("reg_opcode", 3);
+    //     let reg_memory = FieldDefinition::new("reg_memory", 3);
+    //     modrm.add_subfield_definition(6..=7, r#mod);
+    //     modrm.add_subfield_definition(3..=5, reg_opcode);
+    //     modrm.add_subfield_definition(0..=2, reg_memory);
 
-        let modrm = FieldDefinition::new("mod_rm", 8);
-        let r#mod = FieldDefinition::new("mod", 2);
-        let reg_opcode = FieldDefinition::new("reg_opcode", 3);
-        let reg_memory = FieldDefinition::new("reg_memory", 3);
-        // modrm.add_subfield_definition(6..=7, r#mod);
-        // modrm.add_subfield_definition(3..=5, reg_opcode);
-        // modrm.add_subfield_definition(0..=2, reg_memory);
+    //     let opcode8 = FieldDefinition::new("opcode8", 8);
+    //     let imm16 = FieldDefinition::new("imm16", 16);
 
-        let opcode8 = FieldDefinition::new("opcode8", 8);
-        let imm16 = FieldDefinition::new("imm16", 16);
+    //     let mut fields = HashMap::new();
+    //     fields.insert("opcode8", opcode8);
+    //     fields.insert("mod_rm", modrm);
+    //     fields.insert("imm16", imm16);
 
-        let push_imm_16 = create_push_imm16(&opcode8, &modrm, &r#mod, &reg_opcode, &reg_memory, &imm16);
+    //     // x86_isa.add_field_definition("opcode8", opcode8);
+    //     // x86_isa.add_field_definition("mod_rm", modrm);
+    //     // x86_isa.add_field_definition("imm16", imm16);
 
-        let mut push = InstructionDefinition::new("push");
+    //     let push_imm_16 = create_push_imm16(&fields);
 
-        push.add_variant_definition(push_imm_16);
+    //     //let mut push = InstructionDefinition::new("push");
 
-        x86_isa.add_instruction_definition(push);
-        x86_isa
-    }
+    //     //push.add_variant_definition(push_imm_16);
 
-    #[test]
-    fn reserved_opcode_fails() {
-        let isa = create_isa();
-        println!("{:#?}", isa);
-        assert_eq!(isa.disassemble(RESERVED_OPCODE), Err(DisassemblyError::UndefinedInstruction));
-    }
+    //     //x86_isa.add_instruction_definition(push);
+    //     //x86_isa
+    //     (fields, push_imm_16)
+    // }
 
-    #[test]
-    fn push_imm_16() {
-        let isa = create_isa();
-        println!("{:#?}", isa);
-        assert!(isa.disassemble(PUSH_IMM_16).is_ok());
-    }
+    // #[test]
+    // fn reserved_opcode_fails() {
+    //     let (fields, push_imm_16) = create_isa();
+    //     println!("{:#?}", fields);
+    //     println!("{:#?}", push_imm_16);
+    //     assert_eq!(push_imm_16.try_disassemble(RESERVED_OPCODE), Err(DisassemblyError::UndefinedInstruction));
+    // }
+
+    // #[test]
+    // fn push_imm_16() {
+    //     let (fields, push_imm_16) = create_isa();
+    //     println!("{:#?}", fields);
+    //     println!("{:#?}", push_imm_16);
+    //     assert!(push_imm_16.try_disassemble(PUSH_IMM_16).is_ok());
+    // }
 }
