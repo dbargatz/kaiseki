4d5eb86708b35224a785e050bc5710b62bc38875
Dylan Bargatze <dbargatz@users.noreply.github.com>
2023-12-27 23:14:56 +0000

    Create kaiseki-ui crate, move UI functionality there.

---
+++ kaiseki-ui/src/lib.rs
@@ -0,0 +1,79 @@
+use egui::{ColorImage, TextureFilter, TextureOptions};
+use kaiseki_core::Vex;
+use tokio::sync::oneshot::Sender;
+use thiserror::Error;
+
+
+#[derive(Debug, Error)]
+pub enum UiError {
+    #[error("could not create native UI")]
+    CreationError,
+}
+
+pub type Result<T> = std::result::Result<T, UiError>;
+
+pub struct KaisekiUiApp {
+    pub vex: Vex,
+    pub start_tx: Option<Sender<bool>>,
+}
+
+impl KaisekiUiApp {
+    pub fn new(
+        vex: Vex,
+        start_tx: Sender<bool>,
+    ) -> Self {
+        Self {
+            vex,
+            start_tx: Some(start_tx),
+        }
+    }
+}
+
+
+impl eframe::App for KaisekiUiApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        ctx.set_visuals(egui::Visuals::dark());
+
+        let (width, height, frame) = self.vex.get_frame();
+        let image = ColorImage::from_rgb([width, height], &frame);
+        let options = TextureOptions {
+            magnification: TextureFilter::Nearest,
+            minification: TextureFilter::Nearest,
+        };
+        let texture = ctx.load_texture("display", image, options);
+
+        egui::Window::new("Kaiseki")
+            .collapsible(false)
+            .default_size((64.0 * 8.0, 32.0 * 8.0))
+            .resizable(false)
+            .show(ctx, |ui| {
+                ui.image((texture.id(), [width as f32 * 8.0, height as f32 * 8.0].into()));
+                ui.label(format!("Frame number: {:?}", ctx.frame_nr()));
+                ui.allocate_space(ui.available_size());
+            });
+
+        if self.start_tx.is_some() && ctx.frame_nr() > 0 {
+            let start_tx = std::mem::take(&mut self.start_tx).unwrap();
+            let _ = start_tx.send(true);
+        }
+    }
+}
+
+pub fn create_ui(vex: Vex, start_tx: Sender<bool>) -> Result<()> {
+    let options = eframe::NativeOptions::default();
+    let res = eframe::run_native(
+        "Kaiseki",
+        options,
+        Box::new(|cc| {
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+            Box::new(KaisekiUiApp::new(vex, start_tx))
+        }),
+    );
+    match res {
+        Ok(_) => Ok(()),
+        Err(err) => {
+            tracing::error!("error creating native UI: {}", err);
+            Err(UiError::CreationError)
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use clap::{Parser, ValueEnum};
 
-use eframe::CreationContext;
-use egui::{ColorImage, TextureFilter, TextureOptions};
 use kaiseki_chip8::machine::Chip8Machine;
 use kaiseki_core::Vex;
-use tokio::sync::oneshot::Sender;
+use kaiseki_ui::create_ui;
 use tracing_flame::FlameLayer;
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
@@ -20,75 +18,11 @@ struct Args {
     machine: SupportedMachines,
 }
 
-struct KaisekiApp {
-    args: Args,
-    vex: Vex,
-    start_tx: Option<Sender<bool>>,
-}
-
-impl eframe::App for KaisekiApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        ctx.set_visuals(egui::Visuals::dark());
-
-        let (width, height, frame) = self.vex.get_frame();
-        let image = ColorImage::from_rgb([width, height], &frame);
-        let options = TextureOptions {
-            magnification: TextureFilter::Nearest,
-            minification: TextureFilter::Nearest,
-        };
-        let texture = ctx.load_texture("display", image, options);
-
-        let title = format!("{:?} Display", self.args.machine);
-        egui::Window::new(title)
-            .collapsible(false)
-            .default_size((64.0 * 8.0, 32.0 * 8.0))
-            .resizable(false)
-            .show(ctx, |ui| {
-                ui.image(texture.id(), [width as f32 * 8.0, height as f32 * 8.0]);
-                ui.label(format!("Frame number: {:?}", ctx.frame_nr()));
-                ui.allocate_space(ui.available_size());
-            });
-
-        if self.start_tx.is_some() && ctx.frame_nr() > 0 {
-            let start_tx = std::mem::take(&mut self.start_tx).unwrap();
-            let _ = start_tx.send(true);
-        }
-    }
-}
-
-impl KaisekiApp {
-    pub fn new(
-        _creation_ctx: &CreationContext,
-        args: Args,
-        vex: Vex,
-        start_tx: Sender<bool>,
-    ) -> Self {
-        Self {
-            args,
-            vex,
-            start_tx: Some(start_tx),
-        }
-    }
-}
-
 fn create_tokio_runtime() -> tokio::runtime::Runtime {
     tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()
         .unwrap()
-}
-
-fn create_ui(args: Args, vex: Vex, start_tx: Sender<bool>) -> Result<()> {
-    let options = eframe::NativeOptions::default();
-    let res = eframe::run_native(
-        "Kaiseki",
-        options,
-        Box::new(|cc| Box::new(KaisekiApp::new(cc, args, vex, start_tx))),
-    );
-    match res {
-        Ok(_) => Ok(()),
-        Err(_) => Err(anyhow!("Could not set up graphics context")),
-    }
 }
 
 fn main() -> Result<()> {
@@ -104,7 +38,7 @@ fn main() -> Result<()> {
     };
 
     let (start_tx, start_rx) = tokio::sync::oneshot::channel::<bool>();
-    let uiguest = guest.clone();
+    let ui_guest = guest.clone();
 
     tracing::info!("starting emulator thread");
     let emulator_thread = std::thread::spawn(move || {
@@ -116,7 +50,7 @@ fn main() -> Result<()> {
     });
 
     tracing::info!("creating ui");
-    create_ui(args, uiguest, start_tx)?;
+    create_ui(ui_guest, start_tx)?;
 
     tracing::info!("waiting for emulator thread");
     let _ = emulator_thread.join();
